d9c471e9e240fa61cdac7558e89dcb8b98ad2e38
Sorawit Trutsat <sorawit.tr@gmail.com>
2019-06-04 11:31:44 +0700

    edit server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express')
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html")
+})
+app.listen(port)
+
+console.log("Server started.....")
